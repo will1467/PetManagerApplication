9c159fafdbe94513697c6e005957f69847ae4893
will1467 <williamawrigley@gmail.com>
2017-10-05 18:27:55 +1300

    Added pop-up box for pet creation and pet class

--- app/src/main/java/com/example/williamrussa3/petmanagerapplication/MainActivity.java
+++ app/src/main/java/com/example/williamrussa3/petmanagerapplication/MainActivity.java
@@ -1,30 +1,81 @@
 package com.example.williamrussa3.petmanagerapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.mainlayout);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = inflater.inflate(R.layout.add_pet_layout,null);
+
+                final PopupWindow mPopUpWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                mPopUpWindow.showAtLocation(mCoordinatorLayout, Gravity.CENTER,0,0);
+
+                setContentView(R.layout.add_pet_layout);
+
+                final EditText mPetNameEditText = (EditText) findViewById(R.id.petName);
+                mPetNameEditText.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        v.setFocusable(true);
+                        v.requestFocus();
+                        return false;
+                    }
+                });
+
+                final EditText mPetBreedEditText = (EditText) findViewById(R.id.petBreed);
+                mPetBreedEditText.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        v.setFocusable(true);
+                        v.requestFocus();
+                        return false;
+                    }
+                });
+
+                Button mSubmitButton = (Button) findViewById(R.id.submitButton);
+                mSubmitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Pet newPet = new Pet(mPetNameEditText.getText().toString(),mPetBreedEditText.getText().toString());
+                        mPopUpWindow.dismiss();
+                    }
+                });
+
             }
+
         });
     }
 
---
+++ app/src/main/java/com/example/williamrussa3/petmanagerapplication/Pet.java
@@ -0,0 +1,12 @@
+package com.example.williamrussa3.petmanagerapplication;
+public class Pet {
+
+    private String _name;
+    private String _breed;
+
+
+    public Pet(String petName, String petBreed){
+        _name = petName;
+        _breed = petBreed;
+    }
+}
