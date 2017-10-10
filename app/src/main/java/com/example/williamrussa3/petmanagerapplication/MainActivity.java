a95e85d2b6e587c75779ae55d9309b98d53fca92
will1467 <williamawrigley@gmail.com>
2017-10-10 21:39:31 +1300

    pets save + pets can be deleted

--- app/src/main/java/com/example/williamrussa3/petmanagerapplication/MainActivity.java
+++ app/src/main/java/com/example/williamrussa3/petmanagerapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.williamrussa3.petmanagerapplication;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.util.ArraySet;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -21,6 +25,8 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
@@ -29,13 +35,24 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
-    private CoordinatorLayout mCoordinatorLayout;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private ArrayList<Pet> PetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +61,89 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton addPetFab = (FloatingActionButton) findViewById(R.id.add_pet);
+        addPetFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InitialisePopUpWindow();
+                InitialiseAddPetPopUpWindow();
             }
 
         });
+
+        FloatingActionButton deletePetFab = (FloatingActionButton) findViewById(R.id.delete_pet);
+        deletePetFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InitialiseDeletePetPopUpWindow();
+            }
+        });
+
+        PetList = getPetListFromPreferences();
+
+        for(int i = 0; i < PetList.size(); i++){
+            CreatePetIcon(PetList.get(i));
+        }
     }
 
-    private void InitialisePopUpWindow() {
+    private void InitialiseDeletePetPopUpWindow() {
+
+        ArrayList<String> PetNames = new ArrayList<String>();
+
+        for(int i =0; i <PetList.size(); i++){
+            Pet pet = PetList.get(i);
+            PetNames.add(pet.GetName());
+        }
+
+
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customView = inflater.inflate(R.layout.delete_pet_layout,null);
+
+        final PopupWindow mPopUpWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        mPopUpWindow.setFocusable(true);
+        mPopUpWindow.update();
+
+        mPopUpWindow.showAtLocation(customView, Gravity.CENTER,0,0);
+
+        Spinner spinner = (Spinner) customView.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,PetNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        SharedPreferences mPrefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(PetList);
+        prefsEditor.putString("pet_array", json);
+        prefsEditor.commit();
+    }
+
+    private ArrayList<Pet> getPetListFromPreferences() {
+        SharedPreferences mPref = getPreferences(Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String jsonData = mPref.getString("pet_array","");
+
+        if(jsonData.isEmpty()){
+            return new ArrayList<Pet>();
+        }
+
+        Type type = new TypeToken<ArrayList<Pet>>(){}.getType();
+        ArrayList<Pet> Pets = gson.fromJson(jsonData,type);
+        return Pets;
+    }
+
+
+    private void InitialiseAddPetPopUpWindow() {
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View customView = inflater.inflate(R.layout.add_pet_layout,null);
 
@@ -82,6 +171,8 @@ public class MainActivity extends AppCompatActivity {
                 if (mPetBreedEditText.getText() != null && mPetNameEditText.getText() != null) {
                     Pet newPet = new Pet(mPetNameEditText.getText().toString(), mPetBreedEditText.getText().toString());
                     CreatePetIcon(newPet);
+                    PetList.add(newPet);
+
                 }
             }
         });
@@ -96,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
         petIcon.setAdjustViewBounds(true);
         petIcon.setMaxWidth( (linearLayout.getWidth() / 3));
         petName.setText(newPet.GetName());
-        petName.setTextSize(15.0F);
+        petName.setTextSize(30.0F);
         linearLayout.addView(petName);
         linearLayout.addView(petIcon);
 
@@ -131,5 +222,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selectedItem = (String) parent.getItemAtPosition(position);
+
+        for(int i = 0; i < PetList.size(); i++ ){
+            Pet pet = PetList.get(i);
+            if(pet.GetName().equals(selectedItem)){
+                PetList.remove(i);
+            }
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
